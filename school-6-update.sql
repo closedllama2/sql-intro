6f2670ae50ccf30d666e95fd963dcfd150e1c456
Carlo Fava <carlo.fava@kellogg.northwestern.edu>
2023-04-07 21:20:28 +0000

    updating data

---
+++ school-6-update.sql
@@ -0,0 +1,5 @@
+Update students 
+SEt email = "carlo.fava@gmail.com"
+WHERE id = 1;
+
+--ALWAYS USE WHERE STATEMENT WHEN UPDATING
---
+++ school-8-insert-again.sql
@@ -0,0 +1,6 @@
+INSERT INTO students (
+    first_name,
+    last_name,
+    email
+)
+VALUES ("Jane","DOE","jane.doe@gmail.com");
